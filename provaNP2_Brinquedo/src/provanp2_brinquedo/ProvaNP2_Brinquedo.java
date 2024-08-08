e40ca84a6bf840d17b4a91bb1e7bea2fd22a45da
Matheus Tavares <matheustsf18@gmail.com>
2024-08-08 12:21:17 -0300

    Prova Np2 Uniplan

---
+++ provaNP2_Brinquedo/src/provanp2_brinquedo/ProvaNP2_Brinquedo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package provanp2_brinquedo;
+
+/**
+ * Eduardo José     RA 02410039642
+ * Gabriel Costa    RA 02410039484
+ * Gianluca Mendes  RA 02410039649
+ * Matheus Tavares  RA 02410039827
+ * Murilo Rodrigues RA 02410039825
+ * Thiago Peixoto   RA 02410039501
+ */
+public class ProvaNP2_Brinquedo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
